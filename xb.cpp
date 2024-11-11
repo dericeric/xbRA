b82349aff26c08aca6d66790d51c16b53c8e1c5d
dericeric <feigift@gmail.com>
2024-11-11 12:10:31 +0800

    Update xb.cpp

--- xb.cpp
+++ xb.cpp
@@ -5,13 +5,18 @@ int clickCount = 10;          // 需要执行的点击次数
 int clickInterval = 20;       // 每次点击之间的间隔（毫秒）
 int clicksRemaining = 0;      // 剩余点击次数 
 int clickX = 0;               // 点击的X坐标                              
+      
+    
+    
+      
+    
 int clickY = 0;               // 点击的Y坐标 
 bool stopClicking = false;    // 控制是否停止点击的标志
 bool clickingActive = false;  // 检查定时器是否已在运行
 
 void SetTooltip(const std::string& message) {
-    // 显示工具提示（替代 AutoHotkey 的 ToolTip 功能）
-    std::cout << "Tooltip: " << message << std::endl;
+    // 显示工具提示（替代 AutoHotkey 的 ToolTip 功能） 
+    std::cout << "Tooltip: " << message << std::endl;                              
 }
 
 void DoFixedPositionClick(HWND hwnd) {
@@ -21,48 +26,49 @@ void DoFixedPositionClick(HWND hwnd) {
         return;
     }
 
-    // 模拟鼠标点击（ControlClick 替代）
-    INPUT input = { 0 };
-    input.type = INPUT_MOUSE;
-    input.mi.dx = clickX;
-    input.mi.dy = clickY;
-    input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_ABSOLUTE;
-    SendInput(1, &input, sizeof(INPUT));
-    input.mi.dwFlags = MOUSEEVENTF_LEFTUP | MOUSEEVENTF_ABSOLUTE;
-    SendInput(1, &input, sizeof(INPUT));
+    // 模拟鼠标点击
+    INPUT inputs[2] = {};
+    inputs[0].type = INPUT_MOUSE;
+    inputs[0].mi.dx = clickX * (65535 / GetSystemMetrics(SM_CXSCREEN)); // 转换为绝对坐标
+    inputs[0].mi.dy = clickY * (65535 / GetSystemMetrics(SM_CYSCREEN));
+    inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_ABSOLUTE;
 
+    inputs[1].type = INPUT_MOUSE;
+    inputs[1].mi.dx = inputs[0].mi.dx;
+    inputs[1].mi.dy = inputs[0].mi.dy;
+    inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTUP | MOUSEEVENTF_ABSOLUTE;
+
+    SendInput(2, inputs, sizeof(INPUT));
     clicksRemaining--;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
+    case WM_LBUTTONDOWN: // Shift + 左键
+        if (GetAsyncKeyState(VK_SHIFT) & 0x8000) {
+            if (!clickingActive) {
+                POINT pt;
+                GetCursorPos(&pt);
+                clickX = pt.x;
+                clickY = pt.y;
+                clicksRemaining = clickCount;
+                stopClicking = false;
+                clickingActive = true;
+                SetTimer(hwnd, 1, clickInterval, NULL); // 设置定时器以触发 DoFixedPositionClick
+            }
+        }
+        break;
+
     case WM_KEYDOWN:
         if (GetAsyncKeyState(VK_SHIFT) & 0x8000) {
-            switch (wParam) {
-            case VK_LBUTTON: // Shift + Left Click
-                if (clickingActive == false) {
-                    POINT pt;
-                    GetCursorPos(&pt);
-                    clickX = pt.x;
-                    clickY = pt.y;
-                    clicksRemaining = clickCount;
-                    stopClicking = false;
-                    clickingActive = true;
-                    SetTimer(hwnd, 1, clickInterval, NULL);
-                }
-                break;
-
-            case VK_UP: // Shift + WheelUp
+            if (wParam == VK_UP) { // Shift + WheelUp
                 if (clickCount < 30) clickCount++;
                 else clickCount = 1;
                 SetTooltip("Click count: " + std::to_string(clickCount));
-                break;
-
-            case VK_DOWN: // Shift + WheelDown
+            } else if (wParam == VK_DOWN) { // Shift + WheelDown
                 if (clickCount > 1) clickCount--;
                 else clickCount = 30;
                 SetTooltip("Click count: " + std::to_string(clickCount));
-                break;
             }
         }
         break;
@@ -110,5 +116,5 @@ int main() {
         DispatchMessage(&msg);
     }
 
-    return 0;
+    return 0; 
 }
